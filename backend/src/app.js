6a8e9d1601661c91291f40b95a26a11f5233536b
marmariadev <marmariadev@gmail.com>
2024-02-28 15:48:49 -0300

    Add movie routes to the app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 // /backend/src/app.js
 const express = require('express');
+const movieRoutes = require('./routes/movieRoutes');
 const app = express();
 
+app.use(express.json());
+app.use('/api', movieRoutes);
+
 app.get('/', (req, res) => {
-    console.log('Watchlist API');
+    res.send('Watchlist API');
 });
 
 module.exports = app;
